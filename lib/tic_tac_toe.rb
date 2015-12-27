627e7383112b3651aba821bd7f4bfc2b966c63ed
lianaluci <llgrobstein@gmail.com>
2015-12-27 16:57:32 -0500

    Done.

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,92 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+WIN_COMBINATIONS = 
+  [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [6,4,2]
+  ]
+
+def move(board, location, current_player = "X")
+  board[location.to_i-1] = current_player
+end
+
+def position_taken?(board, location)
+  board[location] != " " && board[location] != ""
+end
+
+def valid_move?(board, position)
+  position.to_i.between?(1,9) && !position_taken?(board, position.to_i-1)
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  if valid_move?(board, input)
+    move(board, input, current_player(board))
+  else
+    turn(board)
+  end
+  display_board(board)
+end
+
+def turn_count(board)
+counter = 0 
+board.each do |i|
+  if i == "X" || i == "O"
+    counter += 1
+    end
+  end
+counter
+end
+
+def current_player(board)
+  turn_count(board) % 2 == 0 ? "X" : "O"
+end
+
+def won?(board)
+  WIN_COMBINATIONS.detect do |position| 
+    board[position[0]] == board[position[1]] && 
+    board[position[1]] == board[position[2]] && 
+    board[position[0]] != " "
+  end
+end
+
+def full?(board)
+  board.all? {|position| position == "X" || position == "O"}
+end
+
+def draw?(board)
+  full?(board) && !won?(board)
+end
+
+def over?(board)
+  won?(board) || full?(board) || draw?(board)
+end
+
+def winner(board)
+    board[won?(board)[0]] if won?(board)
+end
+
+def play(board)
+  until over?(board)
+    turn(board)
+  end
+
+  if won?(board)
+    puts "Congratulations #{winner(board)}!"
+  elsif draw?(board)
+    puts "Cats Game!"
+  end
+end
